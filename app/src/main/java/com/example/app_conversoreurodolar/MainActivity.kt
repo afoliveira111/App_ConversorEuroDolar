30cc70ad55e195a675a25ea7147dc4aa7271065a
Felipe <felipetermometria@hotmail.com>
2023-11-28 16:56:52 -0300

    binding.buttonConverter.setOnClickListener {             val euros = binding.editEuro.text.toString().toDouble()             val dolares = euros * 0.8

--- app/src/main/java/com/example/app_conversoreurodolar/MainActivity.kt
+++ app/src/main/java/com/example/app_conversoreurodolar/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.app_conversoreurodolar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.app_conversoreurodolar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonConverter.setOnClickListener {
+            val euros = binding.editEuro.text.toString().toDouble()
+            val dolares = euros * 0.8
+        }
     }
 }
